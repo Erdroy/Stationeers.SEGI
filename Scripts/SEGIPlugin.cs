227126e1bb3b1220f3380a8717ca007d27889c19
Damian Korczowski <erdroy2@gmail.com>
2020-12-04 23:39:55 +0100

    Initial source code drop

---
+++ Scripts/SEGIPlugin.cs
@@ -0,0 +1,20 @@
+﻿// Stationeers.Addons (c) 2018-2020 Damian 'Erdroy' Korczowski & Contributors
+
+using Stationeers.Addons;
+using UnityEngine;
+
+namespace SEGI.Scripts
+{
+    public class SEGI : IPlugin
+    {
+        public void OnLoad()
+        {
+            Debug.Log("SEGI: Hello, World!");
+
+        }
+
+        public void OnUnload()
+        {
+        }
+    }
+}
